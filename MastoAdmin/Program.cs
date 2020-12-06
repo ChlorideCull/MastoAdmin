ef9742856523df22f5114041216cfaa1e00f89cb
Chloride Cull <chloride@devurandom.net>
2020-12-06 17:14:05 +0100

    initial commit

---
+++ MastoAdmin/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+using MastoAdmin.RemoteApi;
+
+namespace MastoAdmin
+{
+    class Program
+    {
+        static void PrintUsage()
+        {
+            Console.WriteLine("MastoAdmin");
+            Console.WriteLine("Automate crap that doesn't have a proper API");
+            Console.WriteLine();
+            Console.WriteLine("Usage: MastoAdmin <admin domain> <admin username> <admin password> <action> <parameters>");
+            Console.WriteLine();
+            Console.WriteLine("Available actions:");
+            Console.WriteLine("  blockdomain <domain> [public comment] [private comment]");
+            Environment.Exit(1);
+        }
+
+        private static Scraping _scraping;
+        
+        static async Task Main(string[] args)
+        {
+            if (args.Length < 4)
+                PrintUsage();
+
+            var domain = args[0];
+            var username = args[1];
+            var password = args[2];
+            var action = args[3];
+            var actionParameters = args.Skip(4).ToArray();
+            
+            _scraping = new Scraping(new Uri(domain));
+            var login = await _scraping.Login(username, password);
+            if (!login)
+                throw new Exception("Failed to login");
+
+            switch (action)
+            {
+                case "blockdomain":
+                    await BlockDomain(actionParameters);
+                    break;
+                default:
+                    Console.WriteLine($"did not recognize action '{action}'");
+                    break;
+            }
+        }
+
+        static async Task BlockDomain(string[] args)
+        {
+            var block = args[0];
+            var publicComment = args.Length > 1 ? args[1] : "";
+            var privateComment = args.Length > 2 ? args[2] : "";
+
+            await _scraping.BlockDomain(block, publicComment, privateComment);
+            Console.WriteLine($"domain '{block}' blocked");
+        }
+    }
+}
---
+++ MastoAdmin/RemoteApi/Scraping.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace MastoAdmin.RemoteApi
+{
+    public class Scraping
+    {
+        protected CookieContainer _cookieContainer;
+        protected HttpClient _client;
+        protected Uri _domain;
+        
+        public Scraping(Uri domain)
+        {
+            _domain = domain;
+            _cookieContainer = new CookieContainer();
+            var handler = new HttpClientHandler()
+            {
+                CookieContainer = _cookieContainer,
+                UseCookies = true
+            };
+            _client = new HttpClient(handler);
+        }
+
+        protected async Task<string> FetchCsrfToken(Uri url, string formId)
+        {
+            var page = await _client.GetAsync(url);
+            page.EnsureSuccessStatusCode();
+            var doc = new HtmlDocument();
+            doc.Load(await page.Content.ReadAsStreamAsync());
+            return doc.GetElementbyId(formId).ChildNodes
+                .Single(x => x.GetAttributeValue<string>("name", null) == "authenticity_token")
+                .GetAttributeValue("value", null);
+        }
+        
+        public async Task<bool> Login(string username, string password)
+        {
+            /*
+             * GET /auth/sign_in, and grab the meta[name="csrf-token"] value as authenticity_token
+             * POST /auth/sign_in with
+             *  * utf8="✓"
+             *  * authenticity_token=<authenticity_token>
+             *  * user[email]=<username>
+             *  * user[password]=<password>
+             *  * button=""
+             */
+            var csrfToken = await FetchCsrfToken(new Uri(_domain.GetLeftPart(UriPartial.Authority) + "/auth/sign_in"), "new_user");
+            
+            var formData = new FormUrlEncodedContent(new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("utf8", "✓"),
+                new KeyValuePair<string, string>("authenticity_token", csrfToken),
+                new KeyValuePair<string, string>("user[email]", username),
+                new KeyValuePair<string, string>("user[password]", password),
+                new KeyValuePair<string, string>("button", "") 
+            });
+            var login = await _client.PostAsync(new Uri(_domain.GetLeftPart(UriPartial.Authority) + "/auth/sign_in"), formData);
+            var loginResponse = await login.Content.ReadAsStringAsync();
+            return !loginResponse.Contains("<title>Log in");
+        }
+        
+        public async Task BlockDomain(string domain, string publicComment = "", string privateComment = "")
+        {
+            /* GET /admin/domain_blocks/new, and grab the meta[name="csrf-token"] value as authenticity_token
+             * POST /admin/domain_blocks with
+             *  * utf8="✓"
+             *  * authenticity_token=<authenticity_token>
+             *  * domain_block[domain]=<domain>
+             *  * domain_block[severity]="suspend"
+             *  * domain_block[reject_media]=0
+             *  * domain_block[reject_reports]=0
+             *  * domain_block[private_comment]=<privateComment>
+             *  * domain_block[public_comment]=<publicComment>
+             *  * button=""
+             */
+            var csrfToken = await FetchCsrfToken(new Uri(_domain.GetLeftPart(UriPartial.Authority) + "/admin/domain_blocks/new"), "new_domain_block");
+            
+            var formData = new FormUrlEncodedContent(new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("utf8", "✓"),
+                new KeyValuePair<string, string>("authenticity_token", csrfToken),
+                new KeyValuePair<string, string>("domain_block[domain]", domain),
+                new KeyValuePair<string, string>("domain_block[severity]", "suspend"),
+                new KeyValuePair<string, string>("domain_block[reject_media]", "0"),
+                new KeyValuePair<string, string>("domain_block[reject_reports]", "0"),
+                new KeyValuePair<string, string>("domain_block[private_comment]", privateComment),
+                new KeyValuePair<string, string>("domain_block[public_comment]", publicComment),
+                new KeyValuePair<string, string>("button", "") 
+            });
+            var executeBlock = await _client.PostAsync(new Uri(_domain.GetLeftPart(UriPartial.Authority) + "/admin/domain_blocks"), formData);
+            executeBlock.EnsureSuccessStatusCode();
+        }
+    }
+}
